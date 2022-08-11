6e8c56915e08229512e530b97849538badbadcbb
Haris Osmanagic <haris@meroxa.io>
2022-08-11 14:39:43 +0200

    print workload

--- scripts/upload-benchmark.sh
+++ scripts/upload-benchmark.sh
@@ -2,7 +2,10 @@
 KEY_FILE=$1
 TARGET=$2
 
+rm scripts/benchmark
 make scripts/benchmark
+
+rm plugins/noop-dest
 make plugins/noop-dest
 
 rsync -v -e "ssh -i $KEY_FILE" -aR scripts/ workloads/ plugins/ ec2-user@"$TARGET":/home/ec2-user/streaming-benchmarks/
