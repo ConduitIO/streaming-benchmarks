1c35826f939f701df3267ba83c1fbe570c4c242a
Haris Osmanagic <haris@meroxa.io>
2022-08-18 16:51:58 +0200

    testing

---
+++ workloads/30k-file.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+echo "Creating pipeline..."
+PIPELINE_ID=$(
+curl -Ss -X POST 'http://localhost:8080/v1/pipelines' -d '
+{
+    "config":
+    {
+        "name": "perf-test",
+        "description": "Test performance"
+    }
+}' | jq -r '.id'
+)
+
+FILE_SIZE=1KB
+echo "Generating a file of size ${FILE_SIZE}"
+rm -f /tmp/conduit-test-file
+fallocate -l $FILE_SIZE /tmp/conduit-test-file
+
+echo "Creating a generator source (1)..."
+SOURCE_CONN_REQ_1=$(
+jq -n --arg pipeline_id "$PIPELINE_ID" '{
+    "type": "TYPE_SOURCE",
+    "plugin": "builtin:generator",
+    "pipeline_id": $pipeline_id,
+    "config":
+    {
+        "name": "generator-source-1",
+        "settings":
+        {
+            "format.type": "file",
+            "format.options": "/tmp/conduit-test-file",
+            "readTime": "0.1ms",
+            "recordCount": "-1"
+        }
+    }
+}'
+)
+CONNECTOR_ID=$(curl -Ss -X POST 'http://localhost:8080/v1/connectors' -d "$SOURCE_CONN_REQ_1" | jq -r '.id')
+
+
+echo "Creating a generator source (2)..."
+SOURCE_CONN_REQ_2=$(
+jq -n --arg pipeline_id "$PIPELINE_ID" '{
+    "type": "TYPE_SOURCE",
+    "plugin": "builtin:generator",
+    "pipeline_id": $pipeline_id,
+    "config":
+    {
+        "name": "generator-source-2",
+        "settings":
+        {
+            "format.type": "file",
+            "format.options": "/tmp/conduit-test-file",
+            "readTime": "0.1ms",
+            "recordCount": "-1"
+        }
+    }
+}'
+)
+CONNECTOR_ID=$(curl -Ss -X POST 'http://localhost:8080/v1/connectors' -d "$SOURCE_CONN_REQ_2" | jq -r '.id')
+
+echo "Creating a generator source (3)..."
+SOURCE_CONN_REQ_3=$(
+jq -n --arg pipeline_id "$PIPELINE_ID" '{
+    "type": "TYPE_SOURCE",
+    "plugin": "builtin:generator",
+    "pipeline_id": $pipeline_id,
+    "config":
+    {
+        "name": "generator-source-3",
+        "settings":
+        {
+            "format.type": "file",
+            "format.options": "/tmp/conduit-test-file",
+            "readTime": "0.1ms",
+            "recordCount": "-1"
+        }
+    }
+}'
+)
+CONNECTOR_ID=$(curl -Ss -X POST 'http://localhost:8080/v1/connectors' -d "$SOURCE_CONN_REQ_3" | jq -r '.id')
+
+echo "Creating a NoOp destination..."
+DEST_CONN_REQ=$(
+jq -n  --arg pipeline_id "$PIPELINE_ID" '{
+     "type": "TYPE_DESTINATION",
+     "plugin": "builtin:file",
+     "pipeline_id": $pipeline_id,
+     "config":
+     {
+         "name": "my-file-destination",
+         "settings": {
+             "path": "/tmp/conduit-perf-test-destination"
+         }
+     }
+ }'
+)
+
+curl -Ss -X POST 'http://localhost:8080/v1/connectors' -d "$DEST_CONN_REQ" > /dev/null
+
+echo "Starting the pipeline..."
+curl -Ss -X POST "http://localhost:8080/v1/pipelines/$PIPELINE_ID/start" > /dev/null
+echo "Pipeline started!"
