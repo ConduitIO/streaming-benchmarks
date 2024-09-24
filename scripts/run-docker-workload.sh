05137a3fcb21762ad96b787f353de309a921d1ac
Haris Osmanagic <haris@meroxa.io>
2024-09-24 13:53:46 +0200

    refactor

--- scripts/run-docker-workload.sh
+++ scripts/run-docker-workload.sh
@@ -12,13 +12,23 @@ docker stop conduit-perf-test || true
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 parent_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-WORKLOAD_PIPELINES="$parent_dir/$WORKLOAD/pipelines"
 WORKLOAD_DATA="$parent_dir/$WORKLOAD/data"
+WORKLOAD_PIPELINES="$parent_dir/$WORKLOAD/pipelines"
+WORKLOAD_CONNECTORS="$parent_dir/$WORKLOAD/connectors"
 
 echo "workload pipelines $WORKLOAD_PIPELINES"
 echo "workload data $WORKLOAD_DATA"
 
-docker run --name conduit-perf-test --memory 1g --cpus=2 -v "$WORKLOAD_DATA":/app/data -v "$WORKLOAD_PIPELINES":/app/pipelines -p 8080:8080 -d "$CONDUIT_IMAGE"
+docker run \
+  --name conduit-perf-test \
+  --memory 1g \
+  --cpus=2 \
+  -v "$WORKLOAD_DATA":/app/data \
+  -v "$WORKLOAD_PIPELINES":/app/pipelines \
+  -v "$WORKLOAD_CONNECTORS":/app/connectors \
+  -p 8080:8080 \
+  -d "$CONDUIT_IMAGE"
+
 sleep 1
 
 source ${__dir}/run-workload.sh "http://localhost:8080" "$WORKLOAD"
