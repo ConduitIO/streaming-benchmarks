4f2164f962df46db4f6d020d4f43a9281a760989
Raúl Barroso <ra.barroso@gmail.com>
2025-04-02 11:39:16 +0200

    cdc finished

--- benchmarks/postgres-kafka-cdc/kafka-connect/init/init.sh
+++ benchmarks/postgres-kafka-cdc/kafka-connect/init/init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-confluent-hub install --no-prompt debezium/debezium-connector-postgresql:16
+# Postgres 16
+confluent-hub install --no-prompt debezium/debezium-connector-postgresql:2.5.4
