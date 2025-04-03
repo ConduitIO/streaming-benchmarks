5c1594a59116cc040dd0cbcc6d99d391f7b3fac3
Raúl Barroso <ra.barroso@gmail.com>
2025-04-03 16:11:57 +0200

    Revert "more complext table"
    This reverts commit 540e8da71524c0fdf081111e6c5a995897ae4e5a.

--- shared/postgres/docker-entrypoint-initdb.d/create_table_employees.sql
+++ shared/postgres/docker-entrypoint-initdb.d/create_table_employees.sql
@@ -1,18 +1,12 @@
 DROP TABLE IF EXISTS employees;
 DROP SEQUENCE IF EXISTS employees_id_seq;
-
 CREATE TABLE employees (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255),
-    full_time BOOLEAN NOT NULL DEFAULT TRUE,
-    position VARCHAR(100),
-    hire_date DATE NOT NULL,
-    salary DECIMAL(10,
-    updated_at TIMESTAMPTZ DEFAULT NOW(),
-    created_at TIMESTAMPTZ DEFAULT NOW()
+    id int NOT NULL,
+    name varchar(255),
+    full_time bool,
+    updated_at timestamptz,
+    PRIMARY KEY (id)
 );
-
 CREATE SEQUENCE employees_id_seq;
 ALTER TABLE employees ALTER id SET DEFAULT NEXTVAL('employees_id_seq');
 ALTER TABLE employees REPLICA IDENTITY FULL;
